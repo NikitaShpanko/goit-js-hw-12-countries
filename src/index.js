49fc20668f60b800dbe317d20f6cbd11a0a4cf4b
Nikita Shpanko <shpancore@gmail.com>
2021-07-26 02:47:32 +0300

    shows Ukraine only

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+
+import fetchCountries from './js/fetchCountries.js';
+import debounce from 'lodash.debounce';
+
+import showCountry from './templates/country.hbs';
+
+const body = document.querySelector('body');
+
+fetchCountries('ukraine').then(res => {
+  body.insertAdjacentHTML('beforeend', showCountry(res[0]));
+});
